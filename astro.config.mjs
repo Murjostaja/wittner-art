005805298b91268bc647f8fb7e6ea9b760cff675
Murjostaja <github@bimson.de>
2026-07-09 19:54:47 +0200

    x

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-// https://astro.build/config
-export default defineConfig({
-  site: "https://lexingtonthemes.com/",
-  integrations: [tailwind(), sitemap()]
-});
